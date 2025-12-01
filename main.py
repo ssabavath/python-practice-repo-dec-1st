642a29edb5c601bcc6031af8faa1b2a074b89075
unknown <sabavathsandeep346@gmail.com>
2025-12-01 14:35:12 +0100

    update python

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+def main() -> list[int]:
+    """_summary_
+
+    Returns:
+        list[int]: _description_
+    """
+    _list = [random.randit(0,20) for _ in range(50)]
+    
+    return _list
